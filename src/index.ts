e820b9dbaaa2f9c2ca8eadca5f4d7aa63eafe9be
Jon Loucks <j.loucks@comcast.net>
2026-01-16 11:47:38 -0500

    Barrel lightweight verification focusing on integration and signature/contract testing

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
-export { AutoClose } from "contracts-ts/api/AutoClose";
+export { VERSION } from "contracts-ts/version";
+
+export { AutoClose, AutoCloseType, AutoCloseMany, AutoCloseOne, typeToAutoClose } from "contracts-ts/api/AutoClose";
+export { AutoCloseFactory, CONTRACT as AUTO_CLOSE_FACTORY } from "contracts-ts/api/AutoCloseFactory";
 export { AutoOpen } from "contracts-ts/api/AutoOpen";
-export { hasFunctions, isNotPresent, isNumber, isPresent, isString, OptionalType, RequiredType } from "contracts-ts/api/auxiliary/Types";
-export { BindStrategy } from "contracts-ts/api/BindStrategy";
+export { OptionalType, RequiredType } from "contracts-ts/api/auxiliary/Types";
+export { hasFunctions, isNotPresent, isNumber, isPresent, isString } from "contracts-ts/api/auxiliary/Types";
+export { BindStrategy, DEFAULT_BIND_STRATEGY } from "contracts-ts/api/BindStrategy";
 export { Contract, Config as ContractConfig } from "contracts-ts/api/Contract";
 export { Contracts, Config as ContractsConfig } from "contracts-ts/api/Contracts";
+export { ContractException } from "contracts-ts/api/ContractException";
+export { ContractsFactory } from "contracts-ts/api/ContractsFactory";  
 export { Promisor, typeToPromisor } from "contracts-ts/api/Promisor";
-export { CONTRACT as PROMISOR_FACTORY, PromisorFactory } from "contracts-ts/api/PromisorFactory";
+export { PromisorFactory, CONTRACT as PROMISOR_FACTORY } from "contracts-ts/api/PromisorFactory";
 export { Repository } from "contracts-ts/api/Repository";
+export { RepositoryFactory, CONTRACT as REPOSITORY_FACTORY } from "contracts-ts/api/RepositoryFactory";
+export { validateContracts } from "contracts-ts/api/auxiliary/Validate";
 
-export { createContract } from "contracts-ts/impl/ContractFactory.impl";
-export { createContracts } from "contracts-ts/impl/ContractsFactory.impl";
+export { createContract, create as createContractFactory } from "contracts-ts/impl/ContractFactory.impl";
+export { createContracts, create as createContractsFactory } from "contracts-ts/impl/ContractsFactory.impl";
 
 
---
+++ src/test/contracts-ts.test.ts
@@ -0,0 +1,206 @@
+import { strictEqual, ok } from "node:assert";
+
+import { validateContracts, VERSION } from "contracts-ts";
+import { AutoClose, AutoCloseType, AutoCloseMany, AutoCloseOne, typeToAutoClose } from "contracts-ts";
+import { AutoCloseFactory, AUTO_CLOSE_FACTORY } from "contracts-ts";
+import { AutoOpen } from "contracts-ts";
+import { OptionalType, RequiredType } from "contracts-ts";
+import { hasFunctions, isNotPresent, isNumber, isPresent, isString } from "contracts-ts";
+import { BindStrategy, DEFAULT_BIND_STRATEGY } from "contracts-ts";
+import { Contract, ContractConfig } from "contracts-ts";
+import { Contracts, ContractsConfig } from "contracts-ts";
+import { ContractException } from "contracts-ts";
+import { ContractsFactory } from "contracts-ts";
+import { Promisor, typeToPromisor } from "contracts-ts";
+import { PromisorFactory, PROMISOR_FACTORY } from "contracts-ts";
+import { Repository } from "contracts-ts";
+import { RepositoryFactory, REPOSITORY_FACTORY } from "contracts-ts";
+import { createContract, createContractFactory } from "contracts-ts";
+import { createContracts, createContractsFactory } from "contracts-ts";
+
+/** 
+ * Tests for contracts-ts index and version exports
+ * All exported functions and constants must already have been tested in their respective test files
+ * These tests ensure that the index exports are correctly set up and accessible
+ * Also tests the VERSION constant to ensure it matches the version in package.json
+ * If this file fails to compile, it indicates a possible breaking for deployment consumers
+ * @module contracts-ts/tests/contracts-ts.test.ts
+ */
+
+describe('VERSION constant', () => {
+  it('should be a non-empty string', () => {
+    strictEqual(typeof VERSION, 'string', 'VERSION should be of type string');
+    ok(VERSION.length > 0, 'VERSION should not be an empty string');
+  });
+});
+
+describe('Index exports', () => {
+  it('should export all expected members', () => {
+    ok(validateContracts, 'validateContracts should be exported');
+    ok(typeToAutoClose, 'typeToAutoClose should be exported');
+    ok(AUTO_CLOSE_FACTORY, 'AUTO_CLOSE_FACTORY should be exported');
+    ok(DEFAULT_BIND_STRATEGY, 'DEFAULT_BIND_STRATEGY should be exported');
+    ok(hasFunctions, 'hasFunctions should be exported');
+    ok(isNotPresent, 'isNotPresent should be exported');
+    ok(isNumber, 'isNumber should be exported');
+    ok(isPresent, 'isPresent should be exported');
+    ok(isString, 'isString should be exported');
+    ok(ContractException, 'ContractException should be exported');
+    ok(typeToPromisor, 'typeToPromisor should be exported');
+    ok(PROMISOR_FACTORY, 'PROMISOR_FACTORY should be exported');
+    ok(createContract, 'createContract should be exported');
+    ok(createContractFactory, 'createContractFactory should be exported');
+    ok(createContracts, 'createContracts should be exported');
+    ok(createContractsFactory, 'createContractsFactory should be exported');
+  });
+});
+
+describe('createContract function', () => {
+  it('should create a Contract instance', () => {
+    const contractConfig: ContractConfig<number> = {
+      name: 'TestContract',
+      test: isNumber,
+    };
+
+    const contract: Contract<number> = createContract(contractConfig);
+
+    strictEqual(contract.name, 'TestContract', 'Contract name should match the provided config');
+    ok(contract.cast(123), 'Contract should cast a numbertype');
+  });
+});
+
+describe('createContracts function', () => {
+  it('passes validation', () => {
+    const contracts: Contracts = createContracts();
+    using _usingContracts: AutoClose = contracts.open();
+
+    validateContracts(contracts);
+  });
+
+  it('pre-bound contracts are bound', () => {
+    const contracts: Contracts = createContracts();
+    using _usingContracts: AutoClose = contracts.open();
+
+    strictEqual(contracts.isBound(PROMISOR_FACTORY), true, 'PROMISOR_FACTORY should be bound in Contracts');
+    strictEqual(contracts.isBound(REPOSITORY_FACTORY), true, 'REPOSITORY_FACTORY should be bound in Contracts');
+    strictEqual(contracts.isBound(AUTO_CLOSE_FACTORY), true, 'AUTO_CLOSE_FACTORY should be bound in Contracts');
+  });
+
+  it('pre-bound contracts are enforced', () => {
+    const contracts: Contracts = createContracts();
+    using _usingContracts: AutoClose = contracts.open();
+
+    const promisorFactory: PromisorFactory = contracts.enforce(PROMISOR_FACTORY);
+    const repositoryFactory: RepositoryFactory = contracts.enforce(REPOSITORY_FACTORY);
+    const autoCloseFactory: AutoCloseFactory = contracts.enforce(AUTO_CLOSE_FACTORY);
+
+    ok(repositoryFactory, 'Enforced REPOSITORY_FACTORY should not be null or undefined');
+    ok(autoCloseFactory, 'Enforced AUTO_CLOSE_FACTORY should not be null or undefined');
+    ok(promisorFactory, 'Enforced PROMISOR_FACTORY should not be null or undefined');
+  });
+
+});
+
+describe('OptionalType and RequiredType aliases', () => {
+  it('should work as expected', () => {
+    let optionalValue: OptionalType<number>;
+    strictEqual(optionalValue, undefined, 'OptionalType should allow undefined');
+
+    optionalValue = null;
+    optionalValue = undefined
+    optionalValue = 42;
+    strictEqual(optionalValue, 42, 'OptionalType should allow assigned value');
+
+    let requiredValue: RequiredType<number> = 100;
+    // requiredValue = null;
+    // requiredValue = undefined;
+    strictEqual(requiredValue, 100, 'RequiredType should hold assigned value');
+  });
+});
+
+describe('ContractsFactory works', () => {
+  it('can create Contracts instances', () => {
+    const factory: ContractsFactory = createContractsFactory();
+    const config: ContractsConfig = {};
+    const contracts: Contracts = factory.create(config);
+    using _usingContracts: AutoClose = contracts.open();
+
+    ok(contracts, 'Contracts instance should be created by ContractsFactory');
+    validateContracts(contracts);
+  });
+});
+
+describe('AutoOpen interface', () => {
+  it('can implement the interface', () => {
+    const instance: AutoOpen = {
+      open(): RequiredType<AutoClose> {
+        return typeToAutoClose((): void => { });
+      },
+    };
+
+    ok(instance, 'AutoOpen instance should be created');
+    using usingIt = instance.open();
+    ok(usingIt, 'AutoClose instance should be created from open() method');
+  });
+});
+
+describe('AutoClose interface', () => {
+  it('can implement the interface', () => {
+    const instance: RequiredType<AutoClose> = {
+      close(): void {
+        // empty
+      },
+      [Symbol.dispose](): void {
+        // empty
+      },
+    };
+
+    ok(instance, 'AutoClose instance should be created');
+    instance.close();
+    instance[Symbol.dispose]();
+  });
+});
+
+describe('AutoCloseFactory interface', () => {
+  it('can implement the interface', () => {
+    const instance: AutoCloseFactory = {
+      createAutoClose: function (_type: RequiredType<AutoCloseType>): RequiredType<AutoClose> {
+        return undefined as unknown as RequiredType<AutoClose>;
+      },
+      createAutoCloseMany: function (): RequiredType<AutoCloseMany> {
+        return undefined as unknown as RequiredType<AutoCloseMany>;
+      },
+      createAutoCloseOne: function (): RequiredType<AutoCloseOne> {
+        return undefined as unknown as RequiredType<AutoCloseOne>;
+      }
+    };
+    
+    ok(instance, 'AutoCloseFactory instance should be created');
+  });
+});
+
+describe('Repository interface', () => {
+  it('can implement the interface', () => {
+    const instance: Repository = {
+      open(): AutoClose {
+        return typeToAutoClose((): void => { });
+      },
+      store<T>(_contract: Contract<T>, _promisor: Promisor<T>, _bindStrategy?: BindStrategy): AutoClose {
+        return typeToAutoClose((): void => { });
+      },
+      keep<T>(_contract: Contract<T>, _promisor: Promisor<T>, _bindStrategy?: BindStrategy): void {
+        // empty
+      },
+      check(): void {
+        // empty
+      },
+      require<T>(_contract: Contract<T>): void {
+        // empty
+      },
+    };
+
+    ok(instance, 'Repository instance should be created');
+    using usingIt = instance.open();
+    ok(usingIt, 'AutoClose instance should be created from open() method');
+  });
+});
